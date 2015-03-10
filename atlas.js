7649a22ee9424e67c51b4d537e8970a431599700
VORA91 <rafivom91@gmail.com>
2015-03-10 11:19:23 +0100

    Create atlas.js

---
+++ atlas.js
@@ -0,0 +1,55 @@
+/*
+	Atlas Project in Multimedia Cartography
+	Institute of Cartography and Geoinformation
+	ETH Zurich
+	Author: H. R. Baer, hbaer@ethz.ch
+	Versions:
+	2015-3-2: Created
+*/
+
+function runPrefixMethod(obj, method) {
+  ['', 'webkit', 'moz', 'o', 'ms'].some(function(pf) {
+    var m = pf + method.charAt(0)[pf.length ? 'toUpperCase' : 'toLowerCase']() + method.slice(1);
+    if (obj[m]) {
+      obj[m]();
+      return true;
+    }
+  });
+};
+
+$('#imprint').button().click(function(event) {
+  $('#dialog' ).dialog({ title: 'Imprint' });
+  $('#dialogframe').prop('src', 'Descriptions/Imprint.html');
+  event.preventDefault();
+});
+
+$('#description').button().click(function() {
+  var url = $('#mapframe').prop('src').replace(/Maps\//, 'Descriptions/').replace(/\..*$/, '.html');
+  $('#dialog').dialog({ title: 'Map Description' });
+  $('#dialogframe').prop('src', url);
+  event.preventDefault();
+});
+
+$('#fullscreen').button().click(function(event) {
+  var element = document.querySelector('body');
+  runPrefixMethod(element, 'requestFullScreen');
+});
+
+$('#tabs').tabs();
+
+$('#accordion').accordion().click(function(event) {
+  switch (event.target.id) {
+  case 'overview':
+  case 'innovative':
+  case 'future':
+    $('#dialog').dialog({ title: 'Not Yet' });
+    $('#dialogframe').prop('src', 'Descriptions/NotYet.html');
+    break;
+  }
+});
+
+$('#autocomplete').autocomplete({
+  source: ['Innovative Map', 'Future Map']
+});
+
+$('#menu-1,#menu-2').menu();
