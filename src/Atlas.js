73139452dc983dad637cdb4f4456243466f69544
agaki <agaki@student.ethz.ch>
2015-04-21 13:39:42 +0200

    Timeslider

--- src/Atlas.js
+++ src/Atlas.js
@@ -294,22 +294,30 @@ $('#autocomplete').autocomplete({
 
 $('#menu-1,#menu-2,#menu-3').menu();
 
-/*Slider-month*/
-	 $(function() {
-$( "#slider-month" ).slider({
-range: "max",
-min: 1,
-max: 12,
-value: 1,
-slide: function( event, ui ) {
-$( "#amount" ).val( ui.value );
-}
-});
+// set up an array to hold the months
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+// lets be fancy for the demo and select the current month.
+var activeMonth = new Date().getMonth();
 
-$( "#amount" ).val( $( "#slider-month" ).slider( "value" ) );
-enableZoom: true;
-
-});
+$(".slider")
+                    
+    // activate the slider with options
+    .slider({ 
+        min: 0, 
+        max: months.length-1, 
+        value: activeMonth 
+    })
+                    
+    // add pips with the labels set to "months"
+    .slider("pips", {
+        rest: "label",
+        labels: months
+    })
+                    
+    // and whenever the slider changes, lets echo out the month
+    .on("slidechange", function(e,ui) {
+        $("#labels-months-output").text( "You selected " + months[ui.value] + " (" + ui.value + ")");
+    });
 
 
 
